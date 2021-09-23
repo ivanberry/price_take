bacce841c35798329af2003f14dd206e0e7262d5
Ivan Teff <tabvim.dev@gmail.com>
2021-09-23 21:23:46 +0800

    通讯

--- public/js/content.js
+++ public/js/content.js
@@ -1,16 +1,17 @@
-var port = chrome.runtime.connect();
-
 window.addEventListener("message", (event) => {
   // We only accept messages from ourselves
-  console.log('event 0: ', event)
+  console.log('message: ', event)
   if (event.source != window) {
     return;
   }
 
-  console.log('event: ', event)
-
   if (event.data.type && (event.data.type == "USER_STORAGE")) {
-    console.log("Content script received: " + event.data.text);
-    port.postMessage(event.data.text);
+    if (chrome.runtime.lastError) {
+      console.log('error')
+    } else {
+      chrome.runtime.onConnect.addListener(port => {
+        port.postMessage('xxxxx')
+      })
+    }
   }
 }, false);
